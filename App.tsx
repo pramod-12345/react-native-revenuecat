5a286faf9d37b9dc37ca5be1d34896982438d90b
Lucky <reactdev@Luckys-MacBook-Air.local>
2024-02-29 19:13:56 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,73 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// Import necessary libraries
+import React, { useEffect } from 'react';
+import { View, Button, Alert, Platform } from 'react-native';
+import { LOG_LEVEL, Purchases } from 'react-native-purchases';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const PaymentScreen = () => {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const loadOfferings = async () => {
+    //AsyncStorage.clear()
+    const offerings = await Purchases.getOfferings();
+    if (offerings?.current?.availablePackages) {
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+      console.log("Offerings: " + JSON.stringify(offerings.current.availablePackages))
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    }
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+  useEffect(() => {
+    const init = async () => {
+      const APIKeys = {
+        apple: 'your_apple_key',
+        google: 'your_google_key'
+      };
+      if (Platform.OS === 'android') {
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+        await Purchases.configure({ apiKey: APIKeys.google });
+      } else {
 
-export default App;
+        await Purchases.configure({ apiKey: APIKeys.apple });
+      }
+
+      // Use more logging during debug if want!
+      Purchases.setLogLevel(LOG_LEVEL.DEBUG);
+
+      // Listen for customer updates
+      Purchases.addCustomerInfoUpdateListener(async (info) => {
+        console.log(' ------> info', info)
+      });
+
+      // Load all offerings and the user object with entitlements
+      await loadOfferings();
+    };
+    init();
+  }, []);
+
+
+  const handlePayment = async () => {
+
+    let productidentifier = 'PRODUCT_IDENTIFIER' // ID for the product which we have mentioned in the app stores.
+
+    // const purchaserInfo = await Purchases.purchasePackage('YOUR_PACKAGE_IDENTIFIER');
+
+    Purchases.purchaseProduct(productidentifier, null, Purchases.PRODUCT_CATEGORY.SUBSCRIPTION)
+      .then((resp) => {
+        console.log(' --Payment Reponse----> ', resp)
+        Alert.alert('Purchase successful')
+      })
+      .catch((error) => {
+        console.log(' ---Payment Error---> ', error)
+        Alert.alert('Purchase failed');
+      }
+      );
+
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Button title="Make Payment" onPress={handlePayment} />
+      </View>
+    );
+
+  }
+
+  export default PaymentScreen;
